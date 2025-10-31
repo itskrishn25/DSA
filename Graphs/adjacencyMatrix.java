76c63682b81edf643510bdbcb08d931ad062c293
itskrishn25 <krishna.048362@tmu.ac.in>
2025-10-31 06:31:36 +0530

    Initial Commit of DSA

---
+++ Graphs/adjacencyMatrix.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class adjacencyMatrix{
+    public static void main(String[] args){
+        // User Input dimenstions of graph
+
+        Scanner obj = new Scanner(System.in);
+
+        int n,m;
+        System.out.println("Enter n and m : ");
+        n = obj.nextInt();
+        m = obj.nextInt();
+
+        int[][] adj = new int[n+1][n+1];
+        for(int i=0;i<m;i++){
+            int u = obj.nextInt();
+            int v = obj.nextInt();
+
+            adj[u][v] = 1;
+            adj[v][u] = 1;
+        }
+
+        printGraph(adj,n,m);
+
+        obj.close();
+    }
+
+    public static void printGraph(int[][] adj, int n,int m){
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<n+1;j++){
+                System.out.print(adj[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
